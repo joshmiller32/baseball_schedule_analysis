9a259104233e3deeb9d2557661a4b38da90a4ec2
Josh Miller <william.miller@att.net>
2020-03-29 11:56:18 -0500

    added url for dataframes

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import streamlit as st
 
 def get_schedule_df():
     columns = [x for x in range(1,31)]
-    schedule_81= pd.read_csv('./Data/81_game_schedule.csv', usecols = columns)
-    schedule_110 = pd.read_csv('./Data/110_game_schedule.csv', usecols = columns)
-    schedule_140 = pd.read_csv('./Data/140_game_schedule.csv', usecols = columns)
-    schedule_162 = pd.read_csv('./Data/162_game_schedule.csv', usecols = columns)
+    url_1 = 'https://github.com/joshmiller32/baseball_schedule_analysis/blob/master/Data/110_game_schedule.csv'
+    url_2 = 'https://github.com/joshmiller32/baseball_schedule_analysis/blob/master/Data/140_game_schedule.csv'
+    url_3 = 'https://github.com/joshmiller32/baseball_schedule_analysis/blob/master/Data/162_game_schedule.csv'
+    url_4 = 'https://github.com/joshmiller32/baseball_schedule_analysis/blob/master/Data/81_game_schedule.csv'
+    schedule_81= pd.read_csv(url_4, usecols = columns)
+    schedule_110 = pd.read_csv(url_1, usecols = columns)
+    schedule_140 = pd.read_csv(url_2, usecols = columns)
+    schedule_162 = pd.read_csv(url_3, usecols = columns)
     dataframes_names = [schedule_81, schedule_110, schedule_140, schedule_162]
     dataframes = []
     for df in dataframes_names:
